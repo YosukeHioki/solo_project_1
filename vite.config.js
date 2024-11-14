11951f3f6c1a6585efc3b138cda0025d828975c0
SUKE <yosuke.hioki@gmail.com>
2024-11-14 22:35:56 +0900

    fixing for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: "./dist",
+      input: "./dist/index.html",
     },
     // server: {
     //   proxy: {
