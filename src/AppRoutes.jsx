644d45f91a6237a214bfee8fb8a32c6dba495bb3
SUKE <yosuke.hioki@gmail.com>
2024-11-14 19:17:37 +0900

    fixing for deploy

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Provider } from "jotai";
+// import { Provider } from "jotai";
 import { Route, Routes } from "react-router-dom";
 
 import { App } from "./App";
 
 export function AppRoutes() {
   return (
-    <Provider>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </Provider>
+    // <Provider>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+    // {/*</Provider>*/}
   );
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./AppRoutes";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>,
+  // <BrowserRouter>
+  <AppRoutes />,
+  // {/*</BrowserRouter>,*/}
 );
